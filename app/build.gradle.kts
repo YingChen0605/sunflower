25219965a43c6fe919a767073c7cb822bd1d1866
yingchen <yingchen@yelp.com>
2023-04-27 16:41:28 -0700

    Warm up Compose in app startup lib and update baseline-prof.txt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -119,6 +119,7 @@ dependencies {
   implementation(libs.androidx.paging.compose)
   implementation(libs.androidx.paging.runtime.ktx)
   implementation(libs.androidx.room.ktx)
+  implementation(libs.androidx.startup.runtime)
   implementation(libs.androidx.work.runtime.ktx)
   implementation(libs.material)
   implementation(libs.gson)
